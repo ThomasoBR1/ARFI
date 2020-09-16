28e0750160810a96283a7eeb35c92f783dfcb2dd
ThomasoBR1 <57733129+ThomasoBR1@users.noreply.github.com>
2020-09-16 08:43:22 -0300

    al

--- arfi/lib/main.dart
+++ arfi/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -6,29 +8,36 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
+  
   runApp(MyApp());
 }
 
-var valCamp = FirebaseFirestore.instance.collection('Light').doc('doc');
-//var estado = valCamp.get().then((value) => {print(value.data['turnOff'])});
+  var valCamp = FirebaseFirestore.instance.collection('Light').doc('doc');
+ // var estado = valCamp.get().then((value) => null);
+  
 
 class MyApp extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
+
+    
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'A.R.F.I.',
+      title: 'A.R.F.I',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'A.R.F.I.'),
+      home: MyHomePage(title: 'A.R.F.I'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
+
 
   final String title;
 
@@ -37,27 +46,33 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _switchValue = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+       
         title: Text(widget.title),
       ),
       body: Center(
+       
         child: Column(
+         
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RaisedButton(
-                child: const Text('Ligar'),
-                onPressed: () {
-                  FirebaseFirestore.instance
-                      .collection('Light')
-                      .doc('doc')
-                      .update({'turnOff' : true});
-                }),
+          Switch(value: _switchValue, onChanged: (value){
+            setState(() {
+              _switchValue = value;
+              FirebaseFirestore.instance.collection('Light').doc('doc').update({'turnOff':_switchValue});
+            });
+          })
+
+
           ],
         ),
       ),
+      
     );
   }
 }
