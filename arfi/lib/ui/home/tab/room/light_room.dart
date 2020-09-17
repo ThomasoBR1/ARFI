1e505b44896c5a00dbbc58af45525d9c71b09fe7
ThomasoBR1 <57733129+ThomasoBR1@users.noreply.github.com>
2020-09-17 16:30:13 -0300

    changes

--- arfi/lib/ui/home/tab/room/light_room.dart
+++ arfi/lib/ui/home/tab/room/light_room.dart
@@ -8,35 +8,64 @@ class LuzQuarto extends StatefulWidget {
 
 class _LuzQuartoState extends State<LuzQuarto> {
   bool _switchValue = false;
+  bool _switchValues = false;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 32, left: 16),
-            child: Text(
-              'Luz da Sala',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                fontStyle: FontStyle.italic,
-              ),         
+    return Scaffold(
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 32, left: 16),
+              child: Text(
+                'Luz da Sala',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
             ),
-          ),
-          Switch(value: _switchValue,
-          onChanged: (value){
-            setState((){
-              _switchValue = value;
-              FirebaseFirestore.instance
-                        .collection('Light')
-                        .doc('doc')
-                        .update({'turnOff': _switchValue});
-            });
-          })
-        ],
+            Switch(
+              value: _switchValue,
+              onChanged: (value) {
+                setState(() {
+                  _switchValue = value;
+                  FirebaseFirestore.instance
+                      .collection('smart_home')
+                      .doc('light')
+                      .update({'living_room': _switchValue});
+                });
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 2, left: 16),
+              child: Text(
+                'Luz do Quarto',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+              Switch(
+              value: _switchValues,
+              onChanged: (value) {
+                setState(() {
+                  _switchValues = value;
+                  FirebaseFirestore.instance
+                      .collection('smart_home')
+                      .doc('light')
+                      .update({'room': _switchValues});
+                });
+              },
+            ),
+
+          ],
+        ),
       ),
     );
   }
